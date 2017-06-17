78e50cb95b78cdcbb8630e5b0ad8e0b932298fa4
Kaustav Majumder <pupunmajumder@gmail.com>
2017-06-18 00:06:59 +0530

    First release

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-
+import { HttpModule } from '@angular/http';
 import  { UsersPage } from '../pages/users/users';
 import { ReposPage } from '../pages/repos/repos';
 import { OrganisationsPage } from '../pages/organisations/organisations';
-
+import { UserDetailsPage } from '../pages/user-details/user-details';
 
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { GithubUsers } from '../providers/github-users/github-users';
 
 @NgModule({
   declarations: [
     MyApp,
 UsersPage,
 ReposPage,
-OrganisationsPage
+OrganisationsPage,
+UserDetailsPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
@@ -28,12 +31,13 @@ OrganisationsPage
     MyApp,
     UsersPage,
     ReposPage,
-    OrganisationsPage
+    OrganisationsPage,
+    UserDetailsPage
   ],
   providers: [
     StatusBar,
-    SplashScreen
-
+    SplashScreen,
+    GithubUsers
   ]
 })
 export class AppModule {}
--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -1,19 +1,39 @@
 import { Component } from '@angular/core';
 import {  NavController } from 'ionic-angular';
-
-
-
+import { GithubUsers} from '../../providers/github-users/github-users'
+import { User } from '../../models/user'
+import { UserDetailsPage } from '../user-details/user-details';
 @Component({
   selector: 'page-users',
   templateUrl: 'users.html',
 })
 export class UsersPage {
+users : User[];
+originalUsers : User[];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private githubUsers : GithubUsers) {
+    githubUsers.load().subscribe(users => {
+      this.users = users;
+      this.originalUsers = users;
+      //console.log(users);
+    })
   }
 
-  ionViewDidLoad() {
-    console.log('Hello UsersPage');
+  gotToDetails(login:String){
+    this.navCtrl.push(UserDetailsPage,{login});
   }
+
+  search(searchEvent){
+    let term = searchEvent.target.value
+    if(term.trim() === ''|| term.trim().length<3){
+      this.users = this.originalUsers;
+    }
+    else{
+      this.githubUsers.searchUsers(term).subscribe(users => {
+        this.users = users
+      });
+    }
+  }
+
 
 }
